22f9b13ac303ffad7eb9cd81881e7ff20c58d3e7
LiuXi0314 <xiaoxixizhizhi@gmail.com>
2017-10-19 18:33:27 +0800

    add file

--- app/src/main/java/com/lx/km/activity/AdActivity.kt
+++ app/src/main/java/com/lx/km/activity/AdActivity.kt
@@ -3,6 +3,7 @@ package com.lx.km.activity
 import android.os.Bundle
 import com.lx.km.R
 import com.lx.km.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_ad.*
 
 class AdActivity : BaseActivity() {
 
@@ -10,7 +11,11 @@ class AdActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ad)
+        closeIcon.setOnClickListener { v ->
+            {
 
+            }
+        }
     }
 
 
